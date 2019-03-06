7293b06f83b436951e4fc7ffc4b147be54696564
Ezhil <sayezhilmaran@gmail.com>
2019-03-06 17:15:06 +0530

    first file14

---
+++ day14.py
@@ -0,0 +1,11 @@
+def checkEVENodd (arr, n, l, r): 
+  
+    if (arr[r] == 1): 
+        print("odd") 
+  
+    else: 
+        print("even") 
+  
+arr = [1, 1, 0, 1] 
+n = len(arr) 
+checkEVENodd (arr, n, 1, 3)
